6f6eca4cce808a48b4c07f8ae3c1e1ce2eccdd5f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-05-26 16:50:29 -0400

    Make slider builder

--- widget/gen/slider.lua
+++ widget/gen/slider.lua
@@ -1,17 +1,34 @@
 local wibox = require("wibox")
 local gears = require("gears")
 local beautiful = require("beautiful")
+local awful = require("awful")
 
-local slider = wibox.widget {
-    bar_shape           = gears.shape.rounded_rect,
-    bar_height          = 3,
-    bar_color           = beautiful.border_color,
-    handle_color        = beautiful.bg_normal,
-    handle_shape        = gears.shape.circle,
-    handle_border_color = beautiful.border_color,
-    handle_border_width = 1,
-    value               = 25,
-    widget              = wibox.widget.slider,
-}
+local function create_slider()
+    local slider = wibox.widget {
+        bar_shape           = gears.shape.rounded_rect,
+        bar_height          = 3,
+        bar_color           = beautiful.border_color,
+        handle_color        = beautiful.bg_normal,
+        handle_shape        = gears.shape.circle,
+        handle_border_color = beautiful.border_color,
+        handle_border_width = 1,
+        value               = 25,
+        widget              = wibox.widget.slider,
+    }
 
-return slider
+    slider:buttons(
+        gears.table.join(
+            awful.button({}, 4, function()
+                slider.value = math.min(slider.value + 5, 100) -- Scroll up: increase value
+            end),
+            awful.button({}, 5, function()
+                slider.value = math.max(slider.value - 5, 0) -- Scroll down: decrease value
+            end)
+        )
+    )
+
+    return slider
+end
+
+
+return create_slider
