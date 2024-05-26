edb217a0bc488609f058ddb53d049dc9e5205890
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-05-25 22:33:06 -0400

    Use slider to change volume

--- widget/volume/init.lua
+++ widget/volume/init.lua
@@ -2,6 +2,9 @@
 local wibox = require("wibox")
 local awful = require("awful")
 local gears = require("gears")
+local beautiful = require("beautiful")
+
+local slider = require("widget.gen.slider")
 
 local HOME = os.getenv('HOME')
 local PATH_TO_ICONS = HOME .. '/.config/awesome/widget/volume/icons/'
@@ -13,48 +16,61 @@ local volume_widget = wibox.widget {
         image = PATH_TO_ICONS .. '0.png', -- Path to volume icon
         widget = wibox.widget.imagebox,
     },
-    {
-        id = "volume_widget",
-        text = "0%",
-        widget = wibox.widget.textbox,
-    },
     layout = wibox.layout.fixed.horizontal,
 }
 
--- Function to update the widget
+local wifi_popup = awful.popup {
+    widget = {
+        {
+            {
+                text   = 'Volume',
+                widget = wibox.widget.textbox
+            },
+            {
+                value         = 0.5,
+                forced_height = 30,
+                forced_width  = 200,
+                widget        = slider,
+            },
+            layout = wibox.layout.fixed.vertical,
+        },
+        margins = 10,
+        widget  = wibox.container.margin
+    },
+    border_color = beautiful.border_color,
+    border_width = 2,
+    ontop = true,
+    visible = false,
+}
+wifi_popup.parent = volume_widget
+
 local function update_volume(widget)
     awful.spawn.easy_async("amixer get Master", function(stdout)
         local volume = string.match(stdout, "(%d?%d?%d)%%")
         volume = tonumber(volume)
-        widget:get_children_by_id("volume_widget")[1].text = volume .. "%"
+        widget.value = volume
     end)
 end
 
--- Set up a timer to update the widget periodically
-gears.timer {
-    timeout = 10,
-    autostart = true,
-    callback = function()
-        update_volume(volume_widget)
-    end
-}
-
--- Update the volume immediately on startup
-update_volume(volume_widget)
-
--- Add mouse buttons to control the volume
 volume_widget:buttons(
     gears.table.join(
-        awful.button({}, 4, function() -- Scroll up to increase volume
-            awful.spawn("amixer set Master 5%+")
-            update_volume(volume_widget)
-        end),
-        awful.button({}, 5, function() -- Scroll down to decrease volume
-            awful.spawn("amixer set Master 5%-")
-            update_volume(volume_widget)
+        awful.button({}, 1, function()
+            awful.placement.next_to(wifi_popup,
+                {
+                    preferred_positions = { "bottom" },
+                    preferred_anchors = { "back" },
+                }
+            )
+            wifi_popup.visible = not wifi_popup.visible
         end)
     )
 )
+
+update_volume(slider)
+slider:connect_signal('property::value', function(widget)
+    awful.spawn("amixer set Master " .. widget.value .. "%")
+end)
+
 
 -- Return the widget so it can be added to the wibox
 return volume_widget
