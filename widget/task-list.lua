96fefb46c6ad9a4dd00250dd80f6975f00864bd6
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-06-01 09:32:12 -0400

    Make task-list component

---
+++ widget/task-list.lua
@@ -0,0 +1,62 @@
+local awful = require("awful")
+local gears = require("gears")
+local wibox = require("wibox")
+
+local tasklist_buttons = gears.table.join(
+    awful.button({}, 1, function(c)
+        if c == client.focus then
+            c.minimized = true
+        else
+            c:emit_signal(
+                "request::activate",
+                "tasklist",
+                { raise = true }
+            )
+        end
+    end),
+    awful.button({}, 3, function()
+        awful.menu.client_list({ theme = { width = 250 } })
+    end),
+    awful.button({}, 4, function()
+        awful.client.focus.byidx(1)
+    end),
+    awful.button({}, 5, function()
+        awful.client.focus.byidx(-1)
+    end))
+
+local function make_task_list(s)
+    return awful.widget.tasklist {
+        screen          = s,
+        filter          = awful.widget.tasklist.filter.currenttags,
+        buttons         = tasklist_buttons,
+        style           = {
+            shape = gears.shape.rounder_bar,
+        },
+        layout          = {
+            spacing = 10,
+            layout  = wibox.layout.fixed.horizontal
+        },
+        -- Notice that there is *NO* wibox.wibox prefix, it is a template,
+        -- not a widget instance.
+        widget_template = {
+            {
+                {
+                    {
+                        id     = 'icon_role',
+                        widget = wibox.widget.imagebox,
+                    },
+                    margins = 2,
+                    widget  = wibox.container.margin,
+                },
+                left   = 1,
+                right  = 1,
+                widget = wibox.container.margin
+
+            },
+            id     = 'background_role',
+            widget = wibox.container.background,
+        }
+    }
+end
+
+return make_task_list
