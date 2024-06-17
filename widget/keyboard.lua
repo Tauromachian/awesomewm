504cd49314a204d3def3f6cd7ffcc8ce93e22304
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-06-17 09:27:21 -0400

    Ref/ Separate set_keyboard_layout function

--- widget/keyboard.lua
+++ widget/keyboard.lua
@@ -15,23 +15,25 @@ local function update_keyboard_layout()
     end)
 end
 
+local function set_keyboard_layout()
+    awful.spawn.easy_async("setxkbmap -query", function(stdout)
+        local layout = stdout:match("layout:%s+(%w+)")
+        local new_layout = ''
+
+        if (layout == 'us') then
+            new_layout = 'es'
+        else
+            new_layout = 'us'
+        end
+
+        awful.spawn("setxkbmap " .. new_layout, false)
+        keyboard_layout.text = " " .. new_layout .. " "
+    end)
+end
+
 keyboard_layout:buttons(
     gears.table.join(
-        awful.button({}, 1, function()
-            awful.spawn.easy_async("setxkbmap -query", function(stdout)
-                local layout = stdout:match("layout:%s+(%w+)")
-                local new_layout = ''
-
-                if (layout == 'us') then
-                    new_layout = 'es'
-                else
-                    new_layout = 'us'
-                end
-
-                awful.spawn("setxkbmap " .. new_layout, false)
-                keyboard_layout.text = " " .. new_layout .. " "
-            end)
-        end)
+        awful.button({}, 1, set_keyboard_layout)
     )
 )
 
