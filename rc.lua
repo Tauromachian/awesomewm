251e3e14c3912306e12e8ab34206176e1a7f1442
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-06-17 20:53:14 -0400

    Move rules into their own file

--- rc.lua
+++ rc.lua
@@ -22,21 +22,12 @@ local make_task_list = require('widget.task-list')
 local make_tag_list = require('widget.tag-list')
 local make_right_widgets = require('widget.right-widgets')
 
+local rules = require("rules")
+
 local startup = require('startup')
 startup()
 
 root.keys(require('keys'))
-
-local clientkeys = require('client.keys')
-local clientbuttons = require('client.buttons')
-
-clientbuttons = gears.table.join(
-    awful.button({ "Mod1" }, 1, function(c)
-        c:emit_signal("request:activate", "mouse_click", { raise = false })
-        awful.mouse.client.move(c)
-    end)
-)
-
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
@@ -235,68 +226,7 @@ root.buttons(gears.table.join(
 
 -- {{{ Rules
 -- Rules to apply to new clients (through the "manage" signal).
-awful.rules.rules = {
-    -- All clients will match this rule.
-    {
-        rule = {},
-        properties = {
-            border_width = beautiful.border_width,
-            border_color = beautiful.border_normal,
-            focus = awful.client.focus.filter,
-            raise = true,
-            keys = clientkeys,
-            buttons = clientbuttons,
-            screen = awful.screen.preferred,
-            placement = awful.placement.no_overlap + awful.placement.no_offscreen
-
-        }
-    },
-
-    -- Floating clients.
-    {
-        rule_any = {
-            instance = {
-                "DTA",   -- Firefox addon DownThemAll.
-                "copyq", -- Includes session name in class.
-                "pinentry",
-            },
-            class = {
-                "Arandr",
-                "Blueman-manager",
-                "Gpick",
-                "Kruler",
-                "MessageWin",  -- kalarm.
-                "Sxiv",
-                "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-                "Wpa_gui",
-                "veromix",
-                "xtightvncviewer" },
-
-            -- Note that the name property shown in xprop might be set slightly after creation of the client
-            -- and the name shown there might not match defined rules here.
-            name = {
-                "Event Tester", -- xev.
-            },
-            role = {
-                "AlarmWindow",   -- Thunderbird's calendar.
-                "ConfigManager", -- Thunderbird's about:config.
-                "pop-up",        -- e.g. Google Chrome's (detached) Developer Tools.
-            }
-        },
-        properties = { floating = true }
-    },
-
-    -- Add titlebars to normal clients and dialogs
-    {
-        rule_any = { type = { "normal", "dialog" }
-        },
-        properties = {}
-    },
-
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
-}
+awful.rules.rules = rules
 -- }}}
 
 -- {{{ Signals
