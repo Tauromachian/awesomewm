5a788a0c5c846c0340b562f9a41e43e7f508084f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-07-06 12:19:39 -0400

    Add flameshot to apps

--- apps.lua
+++ apps.lua
@@ -2,7 +2,8 @@ local apps = {
     bluetooth = 'blueman-manager',
     file_manager = 'nemo',
     password_manager = 'nordpass',
-    vpn = 'windscribe'
+    vpn = 'windscribe',
+    screenshot = 'flameshot gui'
 }
 
 return apps
